526a58ae6bff03fdeca6067b51c7a21a588ea425
Xavier Villanueva <xvillanu@gmail.com>
2016-02-13 10:32:08 -0500

    Update print output

--- JavaHelloWorld.java
+++ JavaHelloWorld.java
@@ -2,7 +2,7 @@ public class JavaHelloWorld
 {
 	public static void main (String [] args)
 	{
-		System.out.println("Java Hello World");
+		System.out.println("Java Hello World!");
 		System.out.println("Hello again");
 	}
 }
